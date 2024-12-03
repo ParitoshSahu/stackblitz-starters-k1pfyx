8d8c231768f042bf87ad73ed517bd6cfc90a04c8
ParitoshSahu <paritoshsahu@karunya.edu.in>
2024-12-03 23:37:15 +0530

    Initial commit

---
+++ models/todos/todo.models.js
@@ -0,0 +1,30 @@
+import { Timestamp } from 'bson';
+import mongoose from 'mongoose';
+
+const todoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    subTodos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'SubTodos',
+      },
+    ], // Array of Sub-Todos
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Todo = mongoose.model('Todo', todoSchema);
